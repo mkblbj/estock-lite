82a599fc81968e1161b32fa4de1bf0ffaf7e5948
mkblbj <mkblbj@gmail.com>
2025-03-15 15:23:21 +0900

    修改页面副标题

--- views/layout/default.blade.php
+++ views/layout/default.blade.php
@@ -22,7 +22,7 @@
 		href="{{ $U('/manifest') . '?data=' . base64_encode($__env->yieldContent('title') . '#' . $U($_SERVER['REQUEST_URI'])) }}">
 	@endif
 
-	<title>@yield('title') | Grocy</title>
+	<title>@yield('title') | UO sotck</title>
 
 	<link href="{{ $U('/packages/@fontsource/roboto/latin.css?v=', true) }}{{ $version }}"
 		rel="stylesheet">
